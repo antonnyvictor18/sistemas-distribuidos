f4f1766b4c8d273f774bcfebdf754449a94edc87
BernardoOCunha <bernardoocunha@poli.ufrj.br>
2023-05-16 18:04:59 -0300

    Add Data

--- SD_TP2/produtor_consumidor.c
+++ SD_TP2/produtor_consumidor.c
@@ -12,19 +12,20 @@ sem_t full;
 sem_t empty;
 
 int consumed = 0;
+
 typedef struct {
     int n;
     int m;
     int *vector;
     int *status_vector;
+    FILE *file;
 } ThreadData;
 
 bool primo(int num) {
     if (num <= 1) {
         return false;
     }
-
-    for (int i = 2; i <= num/2; i++) {
+    for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) {
             return false;
         }
@@ -42,45 +43,46 @@ void write_value(int size, int* vector) {
     }
 }
 
-void read_value(int size, int*vector) {
+int read_value(int size, int*vector, int*consumed) {
     for (int i = 0; i < size; i++) {
         if (vector[i] != 0) {
-            int element = vector[i];
-            if (primo(element)) {
-                printf("Número %d consumido da posicao %d. PRIMO\n", element, i);
-            }
-            else {
-                printf("Número %d consumido da posicao %d. NAO-PRIMO\n", element, i);
-            }
+            int x = vector[i];
             vector[i] = 0;
-            break;
+            (*consumed)++;
+            return x;
         }
     }
 }
 
-void update_status(int* status_vector, int m, int inc_or_dec) {
+int update_status(int* status_vector, int m, int inc_or_dec, FILE* file) {
     for (int i = 0; i < m * 2; i++) {
         if (status_vector[i] == 0) {
             if (i == 0) {
                 status_vector[i] = inc_or_dec;
             }
-            else {
-                status_vector[i] = status_vector[i-1] + inc_or_dec;
+            else if (inc_or_dec == -1) {
+                status_vector[i] = status_vector[i-1] - 1;
             }
+            else {
+                status_vector[i] = status_vector[i-1] + 1;
+            }
+            fprintf(file, "%d %d\n", i, status_vector[i]);
+            return 0;
         }
     }
+    return 0;
 }
 
-bool check_consumed(int* consumed, int m) {
-    if ((*consumed) != m) {
+bool check_consumed(int* consumed, int m, FILE* file) {
+    if ((*consumed) < m) {
         return true;
     }
-    exit(1);
+    fclose(file);
+    exit(0);
 }
 
 void update_consumed(int* consumed) {
     (*consumed)++;
-    printf("%d \n", *consumed);
 }
 
 void* produtor(void* thread_data) {
@@ -89,13 +91,15 @@ void* produtor(void* thread_data) {
     int m = data->m;
     int* vector = data->vector;
     int* status_vector = data->status_vector;
+    FILE* file = data->file;
+    int inc_or_dec = 1;
 
     while(1) {
         sem_wait(&empty);
         sem_wait(&mutex);
-        check_consumed(&consumed, m);
+        check_consumed(&consumed, m, file);
         write_value(n, vector);
-        update_status(status_vector, m, 1);
+        update_status(status_vector, m, inc_or_dec, file);
         sem_post(&mutex);
         sem_post(&full);
     }
@@ -107,25 +111,52 @@ void* consumidor(void* thread_data) {
     int m = data->m;
     int* vector = data->vector;
     int* status_vector = data->status_vector;
+    FILE* file = data->file;
+    int inc_or_dec = -1;
+    int result;
 
     while(1) {
+
         sem_wait(&full);
         sem_wait(&mutex);
-        check_consumed(&consumed, m);
-        read_value(n, vector);
-        update_status(status_vector, m, -1);
-        update_consumed(&consumed);
+        check_consumed(&consumed, m, file);
+        // update_consumed(&consumed);
+        result = read_value(n, vector, &consumed);
+        update_status(status_vector, m, inc_or_dec, file);
         sem_post(&mutex);
         sem_post(&empty);
+
+        // update_consumed(&consumed);
+        if (primo(result)) {
+            printf("Número %d consumido é PRIMO\n", result);
+        }
+        else {
+            printf("Número %d consumido é NAO-PRIMO\n", result);
+        }
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    // Verificar se o número de argumentos está correto
+    if (argc != 4) {
+        printf("O programa deve receber 3 argumentos: n, np, nc");
+        exit(1);
+    }
 
-    int m = 100;
-    int n = 5;
-    int nc = 1;
-    int np = 4;
+
+    int n =  atoi(argv[1]);
+    int np =  atoi(argv[2]);
+    int nc =  atoi(argv[3]);
+    int m = 5000;
+
+    FILE* file = fopen("consumidor_produtor_output.txt", "w");
+    // Check if the file was opened successfully
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return 1;
+    }
+    fprintf(file, "stage fullness\n");
+
 
     // Inicializando o buffer e o buffer que controla o preenchimento deste
     int* vector = (int*)malloc(n * sizeof(int));
@@ -151,34 +182,36 @@ int main() {
         return 1;
     }
 
-    pthread_t threads_consumidoras[nc];
-    ThreadData threads_consumidoras_data[nc];
-    // start_time = clock();
-    for (int i = 0; i < nc; ++i) {
-
-        threads_consumidoras_data[i].n = n;
-        threads_consumidoras_data[i].m = m;
-        threads_consumidoras_data[i].vector = vector;
-        threads_consumidoras_data[i].status_vector = status_vector;
-
-        int result = pthread_create(&(threads_consumidoras[i]), NULL, consumidor, &(threads_consumidoras_data[i]));
-        if (result != 0) {
-            printf("Falha ao Criar Thread COnsumidora.\n");
-            return 1;
-        }
-    }
-
     pthread_t threads_produtoras[np];
     ThreadData threads_produtoras_data[np];
-    // start_time = clock();
+
     for (int i = 0; i < np; ++i) {
 
         threads_produtoras_data[i].n = n;
         threads_produtoras_data[i].m = m;
         threads_produtoras_data[i].vector = vector;
         threads_produtoras_data[i].status_vector = status_vector;
+        threads_produtoras_data[i].file = file; 
 
         int result = pthread_create(&(threads_produtoras[i]), NULL, produtor, &(threads_produtoras_data[i]));
+        if (result != 0) {
+            printf("Falha ao Criar Thread Consumidora.\n");
+            return 1;
+        }
+    }
+
+    pthread_t threads_consumidoras[nc];
+    ThreadData threads_consumidoras_data[nc];
+
+    for (int i = 0; i < nc; ++i) {
+
+        threads_consumidoras_data[i].n = n;
+        threads_consumidoras_data[i].m = m;
+        threads_consumidoras_data[i].vector = vector;
+        threads_consumidoras_data[i].status_vector = status_vector;
+        threads_consumidoras_data[i].file = file;
+
+        int result = pthread_create(&(threads_consumidoras[i]), NULL, consumidor, &(threads_consumidoras_data[i]));
         if (result != 0) {
             printf("Falha ao Criar Thread Consumidora.\n");
             return 1;
